28541529d125620b239cc588d234fe84abe6ceb0
romankhlopyk <romankhlopyk@outlook.com>
2026-04-01 21:25:06 +0300

    v1

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+function getShippingCost(country){
+    let tax;
+    switch (country.toLowerCse()){
+        case 'China': tax = 100;
+        break;
+        case 'Chile': tax = 250;
+        break;
+        case 'Australia': tax = 170;
+        break;
+        case 'Jamaica': tax = 120;
+        break;
+        default:
+            return 'Sorry, there is no delivery to your country'
+    }
+    return `Shipping to ${country} will cost ${price} credits`
+}
+
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
